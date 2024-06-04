22c8d6d5409d11bcc08adba0b17abe551f3f77b8
Nikolai Kotchetkov <motorro@gmail.com>
2024-06-04 17:10:22 +0200

    Generics fix for vertex instructions

--- vertexai/lib/aichat/data/VertexAiSystemInstructions.d.ts
+++ vertexai/lib/aichat/data/VertexAiSystemInstructions.d.ts
@@ -1,22 +1,22 @@
-import { ChatData, SystemInstructions, ToolsDispatcher } from "@motorro/firebase-ai-chat-core";
+import { ChatData, ChatMeta, SystemInstructions, ToolsDispatcher } from "@motorro/firebase-ai-chat-core";
 import { Tool } from "@google-cloud/vertexai/src/types/content";
 /**
  * System instructions for VertexAI
  */
-export interface VertexAiSystemInstructions<DATA extends ChatData> extends SystemInstructions {
+export interface VertexAiSystemInstructions<DATA extends ChatData, CM extends ChatMeta = ChatMeta> extends SystemInstructions {
     /**
      * Tools definition
      */
-    readonly tools?: VertexAiTools<DATA>;
+    readonly tools?: VertexAiTools<DATA, CM>;
 }
 /**
  * VertexAI tools definition
  */
-export interface VertexAiTools<DATA extends ChatData> {
+export interface VertexAiTools<DATA extends ChatData, CM extends ChatMeta = ChatMeta> {
     /**
      * Bound dispatcher
      */
-    readonly dispatcher: ToolsDispatcher<DATA>;
+    readonly dispatcher: ToolsDispatcher<DATA, CM>;
     /**
      * Tools definition
      */
